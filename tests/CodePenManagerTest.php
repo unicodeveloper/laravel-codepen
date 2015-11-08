8bebcd9e0422d07436c65ab209d654d45edca324
busayo <prosperotemuyiwa@gmail.com>
2015-11-08 10:03:02 +0100

    [laravel-codepen] Add boilerplate test

---
+++ tests/CodePenManagerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Unicodeveloper\Codepen\Test;
+
+class CodepenManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
